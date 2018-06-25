d09a7874d06ce1f08c1962ba75f924836c3b4e24
Yann SAN SEBASTIAN <yann@MacBook-Pro-de-Yann.local>
2018-06-25 11:48:39 +0200

    capture access denied

--- capture.php
+++ capture.php
@@ -12,18 +12,25 @@
 </head>
 <body>
     <?php
-        include('navbar.php');        
-        if ($_SESSION['authenticated'] === 'false') {
+        include('navbar.php');
+        if (!isset($_SESSION['authenticated']) || $_SESSION['authenticated'] === 'false') {
+            echo('<article><div class="unauthorized">
+            <p class="code">401</p>
+            <h2>unauthorized access</h2>
+            <h3>Access reserved for connected members</h3>
+        </div></article>');
             include('login.php');
             include('registration.php');
             include('forgotPassword.php');
+        } else {
+            echo('    <div class="wrapper">
+            <article>article</article>
+            <nav>naviguation</nav>
+            <aside>aside</aside>
+        </div>');
         }
     ?>
-    <div class="wrapper">
-        <article>article</article>
-        <nav>naviguation</nav>
-        <aside>aside</aside>
-    </div>
+
     <footer>footer</footer>
 </body>
 
