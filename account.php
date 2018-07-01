dccebd86fbe0340714cf928c9ca69c581b8652b8
Yann SAN SEBASTIAN <ysan-seb@e1r3p10.42.fr>
2018-07-01 16:54:52 +0200

    account 401

--- account.php
+++ account.php
@@ -13,34 +13,45 @@
 <body>
     <?php
         include('navbar.php');        
+        if (!isset($_SESSION['authenticated']) || $_SESSION['authenticated'] === 'false') {
+            echo('<article><div class="unauthorized">
+            <p class="code">401</p>
+            <h2>unauthorized access</h2>
+            <h3>Access reserved for connected members</h3>
+        </div></article>');
+        include('login.php');
+        include('registration.php');
+        include('forgotPassword.php');
+        } else {
         include('notification.php');
         include('username.php');
         include('email.php');
         include('password.php');
+        echo('<div class="wrapper">
+            <article>
+                <div class="accountForm">
+                    <div class="titleForm">Account</div>
+                    <div class="accountProp">
+                        <a style="float:left">Notification setting</a>
+                        <input style="float:right" type="button" onclick="showNotification()" value="Change">
+                    </div>
+                    <div class="accountProp">
+                        <a style="float:left">Username setting</a>
+                        <input style="float:right" type="button" onclick="showUsername()" value="Change">
+                    </div>
+                    <div class="accountProp">
+                        <a style="float:left">Email setting</a>
+                        <input style="float:right" type="button" onclick="showEmail()" value="Change">
+                    </div>
+                    <div class="accountProp">
+                        <a style="float:left">Password setting</a>
+                        <input style="float:right" type="button" onclick="showPassword()" value="Change">
+                    </div>
+                </div>
+            </article>
+        </div>');
+    }
     ?>
-    <div class="wrapper">
-        <article>
-            <div class="accountForm">
-                <div class="titleForm">Account</div>
-                <div class="accountProp">
-                    <a style="float:left">Notification setting</a>
-                    <input style="float:right" type="button" onclick="showNotification()" value="Change">
-                </div>
-                <div class="accountProp">
-                    <a style="float:left">Username setting</a>
-                    <input style="float:right" type="button" onclick="showUsername()" value="Change">
-                </div>
-                <div class="accountProp">
-                    <a style="float:left">Email setting</a>
-                    <input style="float:right" type="button" onclick="showEmail()" value="Change">
-                </div>
-                <div class="accountProp">
-                    <a style="float:left">Password setting</a>
-                    <input style="float:right" type="button" onclick="showPassword()" value="Change">
-                </div>
-            </div>
-        </article>
-    </div>
     <footer>footer</footer>
 </body>
 
